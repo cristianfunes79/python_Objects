3af5973eaba1c7d879be1cffee3ee9772e7efdca
Cristian Funes <cristianfunes79@gmail.com>
2019-07-21 12:57:48 -0300

    Add employer instance

--- firmware/source/main.py
+++ firmware/source/main.py
@@ -1,6 +1,13 @@
 from mathClass import *
+from employerClass import *
 
 #this is working!
 clase_aux = MathClass(2)
 clase_aux.double()
 clase_aux.mult(5)
+
+emp1 = Employer('cristian', 'funes', 120000)
+print(emp1)
+print('{} {}'.format(emp1.first, emp1.last))
+
+print(' fullname: ' + emp1.getFullname())
